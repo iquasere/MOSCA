d6cdf162d3ea9f09697289c84d5ae02dc7e5d584
iquasere <maildosequeira@gmail.com>
2020-10-23 14:57:40 +0100

    Several directories' changes for Bioconda

--- install.bash
+++ install.bash
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+dir="${PREFIX}/share/MOSCA/resources"
+
+# Databases download
+svn export https://github.com/timflutre/trimmomatic/trunk/adapters "${dir}/illumina_adapters"
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+zcat uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz > "${dir}/uniprot.fasta"
+rm uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz
+svn export https://github.com/biocore/sortmerna/trunk/rRNA_databases "${dir}/rRNA_databases"
+find "${dir}/rRNA_databases/*" | grep -v ".fasta" | xargs rm -fr
+
 # Proteomics tools installation
 apt-get install -y libpwiz-tools poppler-utils
 perl ~/anaconda3/opt/krona/install.pl
@@ -7,10 +18,3 @@ wget http://genesis.ugent.be/maven2/eu/isas/searchgui/SearchGUI/3.3.16/SearchGUI
 tar -xzf SearchGUI-3.3.16-mac_and_linux.tar.gz
 wget http://genesis.ugent.be/maven2/eu/isas/peptideshaker/PeptideShaker/1.16.41/PeptideShaker-1.16.41.zip
 unzip PeptideShaker-1.16.41.zip
-
-# Databases download
-svn export https://github.com/timflutre/trimmomatic/trunk/adapters MOSCA/resources/illumina_adapters
-wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
-wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
-zcat uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz > MOSCA/resources/uniprot.fasta
-rm uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz
--- workflow/envs/build.sh
+++ workflow/envs/build.sh
@@ -1,6 +1,25 @@
 dir="${PREFIX}/share/MOSCA"
-mkdir -p "${dir}/scripts" "${dir}/Databases" "${PREFIX}/bin"
-cp scripts/* "${dir}/scripts"
-cp -r Databases "${dir}/Databases"
+mkdir -p "${dir}/scripts" "${PREFIX}/bin"
+cp workflow/scripts/* "${dir}/scripts
+cp workflow/Snakefile workflow/mosca.py "${dir}/scripts
+cp -r resources "${dir}/resources"
 chmod +x "${dir}/scripts/mosca.py"
 ln -s "${dir}/scripts/mosca.py" "${PREFIX}/bin/"
+
+# Databases download
+resources_dir="${PREFIX}/share/MOSCA/resources"
+svn export https://github.com/timflutre/trimmomatic/trunk/adapters "${resources_dir}/illumina_adapters"
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+zcat uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz > "${resources_dir}/uniprot.fasta"
+rm uniprot_trembl.fasta.gz uniprot_sprot.fasta.gz
+svn export https://github.com/biocore/sortmerna/trunk/rRNA_databases "${resources_dir}/rRNA_databases"
+find "${resources_dir}/rRNA_databases/*" | grep -v ".fasta" | xargs rm -fr
+
+# Proteomics tools installation
+apt-get install -y libpwiz-tools poppler-utils
+perl ~/anaconda3/opt/krona/install.pl
+wget http://genesis.ugent.be/maven2/eu/isas/searchgui/SearchGUI/3.3.16/SearchGUI-3.3.16-mac_and_linux.tar.gz
+tar -xzf SearchGUI-3.3.16-mac_and_linux.tar.gz
+wget http://genesis.ugent.be/maven2/eu/isas/peptideshaker/PeptideShaker/1.16.41/PeptideShaker-1.16.41.zip
+unzip PeptideShaker-1.16.41.zip
