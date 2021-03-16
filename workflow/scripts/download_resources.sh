df09eabff1e28a9ad6e97ed14a812d44ea113ca0
iquasere <maildosequeira@gmail.com>
2021-03-16 14:17:38 +0000

    SortMeRNA version

--- workflow/scripts/download_resources.sh
+++ workflow/scripts/download_resources.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-svn export https://github.com/timflutre/trimmomatic/trunk/adapters "$1/adapters"
-svn export https://github.com/biocore/sortmerna/trunk/data/rRNA_databases "$1/rRNA_databases"
-find "$1/rRNA_databases/*" | grep -v ".fasta" | xargs rm -fr
+svn export https://github.com/timflutre/trimmomatic/trunk/adapters "$1/adapters" --force
+svn export https://github.com/biocore/sortmerna/trunk/data/rRNA_databases "$1/rRNA_databases" --force
