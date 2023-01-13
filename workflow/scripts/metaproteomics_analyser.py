811916c46817ad8f6af989a1b7de2c4c26e9f264
iquasere <maildosequeira@gmail.com>
2023-01-13 20:24:59 +0000

    Added UPIMAPI_results.tsv as input for metaproteomics rule

--- workflow/scripts/metaproteomics_analyser.py
+++ workflow/scripts/metaproteomics_analyser.py
@@ -314,7 +314,7 @@ class MetaproteomicsAnalyser:
 
     def run(self):
         args = self.get_arguments()
-        '''
+
         # 1st database construction
         self.database_generation(
             args.database, args.output, args.upimapi_result, contaminants_database=args.contaminants_database,
@@ -325,7 +325,7 @@ class MetaproteomicsAnalyser:
             self.generate_parameters_file(f'{args.output}/1st_params.par', protein_fdr=100)
         except:
             print('An illegal reflective access operation has occurred. But MOSCA can handle it.')
-        '''
+
         # 2nd database construction
         proteins_for_second_search = []
         for i in range(len(args.names)):
